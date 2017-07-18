05a96d4f8401c1ec250a24fed77c79e497e8df07
SukhanovMG <maks14093@list.ru>
2017-07-18 22:47:46 +0300

    Растём

--- main.cpp
+++ main.cpp
@@ -7,9 +7,10 @@ using namespace std;
 int main()
 {
 	myvector<int> v;
-	for (int i = 1; i < 5; i++)
+	for (int i = 0; i < numeric_limits<int>::max(); i++)
 	{
 		v.push_back(i);
+		cout << "size: " << v.size() << "; capacity: " << v.capacity() << endl;
 	}
 
 	for (int i = 0; i < v.size(); i++)
--- myvector.h
+++ myvector.h
@@ -4,11 +4,11 @@ public:
 	myvector()
 		: m_data(nullptr), m_capacity(0), m_size(0)
 	{
-		m_data = new T[8];
-		m_capacity = 8;
+		realloc(8);
 	}
 	~myvector()
 	{
+		delete[] m_data;
 	}
 
 	int capacity() const { return m_capacity; }
@@ -16,6 +16,8 @@ public:
 
 	void push_back(const T& value)
 	{
+		if (m_size == m_capacity)
+			realloc(m_capacity * 2);
 		m_data[m_size++] = value;
 	}
 
@@ -27,6 +29,34 @@ public:
 	T * begin() { return m_data; }
 	T * end() { return m_data + m_size; }
 private:
+	void realloc(int new_capacity)
+	{
+		if (new_capacity <= 0 || new_capacity < m_capacity)
+			return;
+		T* new_data = new T[new_capacity];
+		try
+		{
+			for (int i = 0; i < size(); i++)
+			{
+				new_data[i] = m_data[i];
+			}
+		}
+		catch(...)
+		{
+			delete[] new_data;
+			throw;
+		}
+		m_capacity = new_capacity;
+		try
+		{
+			delete[] m_data;
+		}
+		catch(...)
+		{
+		}
+		m_data = new_data;
+	}
+
 	T* m_data;
 	int m_capacity;
 	int m_size;
